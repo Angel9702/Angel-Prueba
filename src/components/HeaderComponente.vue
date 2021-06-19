e9a3f092241ee5d6dff5fd1e4a062737ce221677
Angel9702 <angelfranquinacuya@gmail.com>
2021-06-18 19:08:27 -0500

    svg

--- src/components/HeaderComponente.vue
+++ src/components/HeaderComponente.vue
@@ -6,23 +6,7 @@
       </svg>
     </div>
     <nav>
-      <!-- <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul> -->
-      <svg xmlns="http://www.w3.org/2000/svg" width="245" height="24" viewBox="0 0 245 24">
-  <g id="Grupo_8437" data-name="Grupo 8437" transform="translate(-1032 -18)">
-    <text class="yury" id="Yury" transform="translate(1203 37)" fill="#fff" font-size="20" font-family="Montserrat-Medium, Montserrat" font-weight="500"><tspan x="0" y="0">Yury</tspan></text>
-    <path id="Polígono_6" data-name="Polígono 6" d="M4.5,0,9,8H0Z" transform="translate(1277 34) rotate(180)" fill="#fff"/>
-    <path id="Trazado_4882" data-name="Trazado 4882" d="M704.646,501.441l0,0a9.985,9.985,0,0,0-7.069-2.907h-.045a10,10,0,1,0-.016,20h.045a10.03,10.03,0,0,0,7.088-17.09Zm-7.1,2.271a2.423,2.423,0,1,1-2.426,2.423A2.428,2.428,0,0,1,697.544,503.711Zm0,6.955a4.973,4.973,0,0,1,4.9,4.131,7.944,7.944,0,0,1-9.8,0A5,5,0,0,1,697.544,510.666Zm3.733-7.049a4.5,4.5,0,1,0-7.464,5.036,4.521,4.521,0,0,0,.57.687,7.038,7.038,0,0,0-3.4,3.705,7.961,7.961,0,1,1,13.111,0,7.047,7.047,0,0,0-3.393-3.7A4.5,4.5,0,0,0,701.277,503.617Z" transform="translate(485.454 -478.531)" fill="#fff"/>
-    <path id="Trazado_5258" data-name="Trazado 5258" d="M-52.322,655.69a2.406,2.406,0,0,0,2.4-2.385.014.014,0,0,0-.013-.014h-4.773a.014.014,0,0,0-.013.014A2.406,2.406,0,0,0-52.322,655.69Zm7.8-7.195,0-.01v-6.6a7.722,7.722,0,0,0-5.99-7.557.012.012,0,0,1-.01-.012v-.83a1.845,1.845,0,0,0-1.8-1.8,1.845,1.845,0,0,0-1.8,1.8v.83a.013.013,0,0,1-.01.012,7.722,7.722,0,0,0-5.99,7.557v6.6l0,.01-2.148,2.147a.848.848,0,0,0-.248.6h0a.848.848,0,0,0,.848.848h18.7a.848.848,0,0,0,.848-.848h0a.853.853,0,0,0-.248-.6Zm-2.417,1.2H-57.709a.013.013,0,0,1-.013-.013v-7.709a5.452,5.452,0,0,1,6.051-5.439,5.376,5.376,0,0,1,4.748,5.361v7.787A.013.013,0,0,1-46.936,649.69Z" transform="translate(1166.521 -613.691)" fill="#fff"/>
-    <path id="Icon_material-mail-outline" data-name="Icon material-mail-outline" d="M24.375,6h-19A2.372,2.372,0,0,0,3.012,8.375L3,22.625A2.382,2.382,0,0,0,5.375,25h19a2.382,2.382,0,0,0,2.375-2.375V8.375A2.382,2.382,0,0,0,24.375,6Zm0,16.625h-19V10.75l9.5,5.938,9.5-5.937Zm-9.5-8.312-9.5-5.937h19Z" transform="translate(1029 15)" fill="#fff"/>
-    <path id="Trazado_17115" data-name="Trazado 17115" d="M6.9,0A6.9,6.9,0,1,1,0,6.9,6.9,6.9,0,0,1,6.9,0Z" transform="translate(1117.199 18)" fill="#722ae9"/>
-    <text id="_1" data-name="1" transform="translate(1124 29)" fill="#fff" font-size="10" font-family="Montserrat-Bold, Montserrat" font-weight="700"><tspan x="-1.96" y="0">1</tspan></text>
-  </g>
-</svg>
+      <img src="../assets/panel.svg" alt="">
     </nav>
   </header>
 </template>
@@ -41,9 +25,9 @@ header {
   justify-content: space-between;
   align-items: center;
 }
-/* .yury {
-  display: none;
-} */
+img {
+  max-width: 200px;
+}
 
 @media (min-width: 576px) { 
   .header {
